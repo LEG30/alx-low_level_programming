e6759db1bfa27e94ddb85544702dbeb622d68346
Lesego <lesegomoeletsi99@gmail.com>
2022-06-06 06:20:26 -0700

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main → Assign random number and print stdout.
  *
  * Return: Always 0 (success) 
  */
 int main(void) 
 {
-	int n,lastDigit;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (lastDigit > 5) 
-	{
-	 printf("%d is greater than 5\n", lastDigit);
-	} 
-	else if (lastDigit < 6 && lastDigit != 0) 
-	{
-	 printf("%d is less than 6 and not 0\n", lastDigit);
-	} 
-	else (lastDigit == 0) 
-	{ 
-	 printf("%d is 0\n", lastDigit);
-	} 
-        return (0);
+	printf("Last digit of %d", n);
+	if (n > 5) 
+{
+	printf("greater than 5");
 } 
+if (n == 0) 
+{
+	printf("0");
+}
+if (n < 6 && n != 0)
+{
+	printf("less than 6 not 0");
+}
+        printf("\n");
+return (0);
+}
 
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main → Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (success) 
+ */
+int main(void) 
+{
+	int ch;
+	for(ch 97; ch <= 122; ch ++) 
+	{
+		putchar(ch);
+	}
+	        putchar(10); /*this is the ascii code for new line*/
+
+		return (0);
+}
+
