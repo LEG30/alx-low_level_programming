1de363201b321815b77235a3fec49b736aeddc17
Lesego <lesegomoeletsi99@gmail.com>
2022-06-06 16:01:45 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main → Prints all single base numbers 0f base 10 starting from .
+ * Return: Always 0
+ */
+int main(void)
+{
+	int(ch);
+	for (ch = 48; ch <= 57, ch++)
+	{
+		putchar (ch);
+
+		putchar (10): /* this is an ascii code for the new line */
+
+		return (0);
+}
+
