fb44b84d91cc546d3693a3e8fa1b874e75790c33
Lesego <lesegomoeletsi99@gmail.com>
2022-06-06 16:48:29 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main → Prints all the numbers of base 16 in lowercase.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+/**
+ *
+ * hexadecimal
+ * 0 1 2 3 4 5 6 7 8 9 a b c d e f
+ */
+	char ch;
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+	} 
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar (ch);
+	} 
+	        putcar (10);
+
+		return (0);
+
+}
+
