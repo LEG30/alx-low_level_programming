c94c05ea1ff819fde9c97f66023921afeebbcef4
Lesego <lesegomoeletsi99@gmail.com>
2022-06-06 15:20:54 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-point_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main → Prints the alphabet in lowercase and excludes letters q and e.
+ * Return: 0 (success)
+ */
+int main(void)
+{ 
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+	} 
+	      putchar(ch);
+} 
+
