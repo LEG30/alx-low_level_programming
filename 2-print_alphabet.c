0e85dd0b32b9f2338c3fe74c5b292e4d3eed81fe
Lesego <lesegomoeletsi99@gmail.com>
2022-06-06 14:39:09 -0700

    Task 2

---
+++ 2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main → Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+	for (ch 97; ch <= 122; ch++)
+	{
+		putchar (ch);
+	}
+	         putchar (10); /* this is an ascii code for the new line */
+
+		 return (0);
+}
+
