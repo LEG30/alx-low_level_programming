4254192a113d329e144ced0bc0ad49d9255e141f
Lesego <lesegomoeletsi99@gmail.com>
2022-06-07 05:13:23 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main → Entry point
+ * Description: Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void);
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"
+	int ch,
+	for(ch = 0; ch < 26; ch++)
+	{
+	_putchar(alphabet[i]);
+	}
+        _putchar("\n");
+
+	return (0);
+}
+
+	
+
