2c0ac6598883effff1b7673656036ed33756c86f
Lesego <lesegomoeletsi99@gmail.com>
2022-06-07 05:49:51 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include main.h
+
+/**
+ * main → Prints 10 times the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void);
+{
+             int i = 0, j;
+
+	     while (i < 10)
+	     {
+              j = 0
+		while (j < 26)
+	     {
+               _putchar(alphabet[j]);
+	       j ++;
+	     }
+	      _putchar("\n");
+	       i ++;
+	     }
+           return (0);
+}
+
+
+
+
+
+
