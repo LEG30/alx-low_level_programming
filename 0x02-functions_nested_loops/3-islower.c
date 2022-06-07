c413f31017066b99ba60c2e9ca3c7d9428c69a1b
Lesego <lesegomoeletsi99@gmail.com>
2022-06-07 06:47:42 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include main.h
+
+/**
+ * main → Checks for lowercase characters.
+ *
+ * Return: Always 0 (success)
+ */
+int _islower(int c);
+{
+	for char in str:
+	c = char.islower()
+	if c == True
+	{
+           print("True");
+        }
+	if (c != 1)
+	{
+	   print("False");
+	} 
+	return (0);
+}
+
