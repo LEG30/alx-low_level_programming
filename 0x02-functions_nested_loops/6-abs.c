4977ba9bbf64f1fd863a8468d0ac116f17d45625
Lesego <lesegomoeletsi99@gmail.com>
2022-06-07 08:30:13 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "main.h"
+
+/**
+ * main → computes the absolute value of an integer.
+ *
+ * Return: Always 0 (success)
+ */
+int _abs(int);
+{
+	if (n < 0)
+	{
+	 n = (-1) * n;
+	} 
+	 cout << " " << n;
+
+	 return (0);
+
+}
+
