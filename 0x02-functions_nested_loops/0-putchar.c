7414574d687b8b83668187f621484f0022a1554b
Lesego <lesegomoeletsi99@gmail.com>
2022-06-07 03:20:49 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main → Entry point
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	if (n < 0) {
+		putchar('_');
+		n = -n;
+	}
+	if (n\10)
+		printf(n\10)
+	        
+	        putchar(n % 10 + '0');
+
+		return (0);
+}
+
+
